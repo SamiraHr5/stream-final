b22b0a0d226a55b479ca903a90609493a03d178e
SamiraHr5 <samirahazimr@gmail.com>
2024-06-15 11:55:11 -0600

    Create Movies.cpp

---
+++ Movies.cpp
@@ -0,0 +1,10 @@
+#include "Movies.hpp"
+#include <iostream>
+
+Movies::Movies(int id, std::string title, int duration, std::string genre, double rating)
+    : Video(id, title, duration, genre, rating) {}
+
+void Movies::showData() const {
+    std::cout << "Movie [ID: " << id << ", Title: " << title << ", Duration: " << duration
+              << " mins, Genre: " << genre << ", Rating: " << rating << "]" << std::endl;
+}
