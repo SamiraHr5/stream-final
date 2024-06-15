b8f09d893b773f308a51dfd615fcdb09f17f0dba
SamiraHr5 <samirahazimr@gmail.com>
2024-06-15 11:53:58 -0600

    Update Episodes.cpp

--- Episodes.cpp
+++ Episodes.cpp
@@ -1,1 +1,10 @@
+#include "Episodes.hpp"
+#include <iostream>
 
+Episodes::Episodes(int id, std::string seriesId, std::string title, int duration, double rating, std::string season)
+    : Video(id, title, duration, "", rating), seriesId(seriesId), season(season) {}
+
+void Episodes::showData() const {
+    std::cout << "Episode [ID: " << id << ", Series ID: " << seriesId << ", Title: " << title
+              << ", Duration: " << duration << " mins, Rating: " << rating << ", Season: " << season << "]" << std::endl;
+}
