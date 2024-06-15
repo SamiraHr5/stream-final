fcfd6457ab528e81f4138550546d351591a8b831
SamiraHr5 <samirahazimr@gmail.com>
2024-06-15 11:52:23 -0600

    Create Video.cpp

---
+++ Video.cpp
@@ -0,0 +1,29 @@
+#include "Video.hpp"
+#include <iostream>
+
+Video::Video(int id, std::string title, int duration, std::string genre, double rating)
+    : id(id), title(title), duration(duration), genre(genre), rating(rating) {}
+
+int Video::getId() const {
+    return id;
+}
+
+std::string Video::getTitle() const {
+    return title;
+}
+
+int Video::getDuration() const {
+    return duration;
+}
+
+std::string Video::getGenre() const {
+    return genre;
+}
+
+double Video::getRating() const {
+    return rating;
+}
+
+void Video::setRating(double rating) {
+    this->rating = rating;
+}
