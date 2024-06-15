2515704100a7acfba437bc5f92613c6783fef65a
SamiraHr5 <samirahazimr@gmail.com>
2024-06-15 11:55:55 -0600

    Create Series.cpp

---
+++ Series.cpp
@@ -0,0 +1,32 @@
+#include "Series.hpp"
+#include <iostream>
+
+Series::Series(int id, std::string title, int seasons)
+    : Video(id, title, 0, "", 0.0), seasons(seasons) {}
+
+Series::~Series() {
+    for (Episodes* episode : episodes) {
+        delete episode;
+    }
+}
+
+int Series::getSeasons() const {
+    return seasons;
+}
+
+void Series::addEpisode(Episodes* episode) {
+    episodes.push_back(episode);
+}
+
+void Series::showData() const {
+    std::cout << "Series [ID: " << id << ", Title: " << title << ", Seasons: " << seasons << "]" << std::endl;
+}
+
+void Series::showEpisodesWRating(double rating) const {
+    std::cout << "Episodes with rating " << rating << ":" << std::endl;
+    for (Episodes* episode : episodes) {
+        if (episode->getRating() == rating) {
+            episode->showData();
+        }
+    }
+}
