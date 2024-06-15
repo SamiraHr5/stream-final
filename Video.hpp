04e2021ab4ef5c2508f0ab8516460d7fd76db7fe
SamiraHr5 <samirahazimr@gmail.com>
2024-06-15 11:52:40 -0600

    Update Video.hpp

--- Video.hpp
+++ Video.hpp
@@ -1,1 +1,30 @@
+#ifndef VIDEO_HPP
+#define VIDEO_HPP
+
+#include <string>
+
+class Video {
+protected:
+    int id;
+    std::string title;
+    int duration;
+    std::string genre;
+    double rating;
+
+public:
+    Video(int id, std::string title, int duration, std::string genre, double rating);
+    virtual ~Video() {}
+
+    int getId() const;
+    std::string getTitle() const;
+    int getDuration() const;
+    std::string getGenre() const;
+    double getRating() const;
+
+    void setRating(double rating);
+
+    virtual void showData() const = 0;
+};
+
+#endif
 
