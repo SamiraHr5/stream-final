6e34a7eb188095902e6edc045bb48982d766c14d
SamiraHr5 <samirahazimr@gmail.com>
2024-06-15 11:55:32 -0600

    Create Series.hpp

---
+++ Series.hpp
@@ -0,0 +1,24 @@
+#ifndef SERIES_HPP
+#define SERIES_HPP
+
+#include "Video.hpp"
+#include <vector>
+#include <string>
+#include "Episodes.hpp"
+
+class Series : public Video {
+private:
+    int seasons;
+    std::vector<Episodes*> episodes;
+
+public:
+    Series(int id, std::string title, int seasons);
+    ~Series();
+
+    int getSeasons() const;
+    void addEpisode(Episodes* episode);
+    void showData() const override;
+    void showEpisodesWRating(double rating) const;
+};
+
+#endif
