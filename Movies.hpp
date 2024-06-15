7b07e61b812cb6333835fe510a8d46da0bf91d60
SamiraHr5 <samirahazimr@gmail.com>
2024-06-15 11:54:46 -0600

    Create Movies.hpp

---
+++ Movies.hpp
@@ -0,0 +1,12 @@
+#ifndef MOVIES_HPP
+#define MOVIES_HPP
+
+#include "Video.hpp"
+
+class Movies : public Video {
+public:
+    Movies(int id, std::string title, int duration, std::string genre, double rating);
+    void showData() const override;
+};
+
+#endif
